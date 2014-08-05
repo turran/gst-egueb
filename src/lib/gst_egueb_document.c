92e5c555b3ddde6f2f567d7d6f03adc69a3950f2
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-08-05 21:47:54 +0200

    Finally load images using a gst pipeline

--- src/lib/gst_egueb_document.c
+++ src/lib/gst_egueb_document.c
@@ -30,37 +30,47 @@ struct _Gst_Egueb_Document
 typedef struct _Gst_Egueb_Document_Pipeline
 {
 	GstElement *pipeline;
+	gboolean done;
+	/* data needed for the data event */
 	Eina_Binbuf *binbuf;
+	/* data needed for the image event */
+	/* the enesim stream mmap */
+	void *stream_data;
+	size_t stream_length;
 	Enesim_Surface *surface;
+	gboolean buffer_pushed;
 } Gst_Egueb_Document_Pipeline;
 
-static gboolean _gst_egueb_document_pipeline_process(GstElement *p)
+static gboolean _gst_egueb_document_pipeline_process(Gst_Egueb_Document_Pipeline *p)
 {
 	GstBus *bus;
-	gboolean done = FALSE;
 
-	bus = gst_pipeline_get_bus(GST_PIPELINE(p));
-	do
+	bus = gst_pipeline_get_bus(GST_PIPELINE(p->pipeline));
+	while (!p->done)
 	{
 		GstMessage *msg;
 		msg = gst_bus_timed_pop (bus, GST_CLOCK_TIME_NONE);
 		switch (GST_MESSAGE_TYPE(msg))
 		{
-			case GST_MESSAGE_ERROR:
-			ERR("Error received on the pipeline");
-			done = TRUE;
+			case GST_MESSAGE_ERROR:{
+			GError *err = NULL;
+			gchar *dbg_info = NULL;
+			gst_message_parse_error(msg, &err, &dbg_info);
+			ERR("Error received on the pipeline '%s', %s'", err->message, dbg_info ? dbg_info : "none");
+			p->done = TRUE;
+			}
 			break;
 
 			case GST_MESSAGE_EOS:
-			INFO("Data completed");
-			done = TRUE;
+			INFO("Pipeline reached EOS");
+			p->done = TRUE;
 			break;
 
 			default:
 			break;
 		}
 		gst_message_unref(msg);
-	} while (!done);
+	}
 	gst_object_unref(bus);
 }
 
@@ -86,16 +96,162 @@ static void _gst_egueb_document_data_uridecodebin_pad_added_cb (
 	g_object_set(sink, "signal-handoffs", TRUE, NULL);
 	/* get the handoff buffer and put it on an enesim stream */
 	g_signal_connect (G_OBJECT (sink), "handoff",
-		G_CALLBACK (_gst_egueb_document_data_fakesink_handoff_cb),
-		p);
-	sinkpad = gst_element_get_static_pad(sink, "sink");
+			G_CALLBACK (_gst_egueb_document_data_fakesink_handoff_cb),
+			p);
+
 	gst_bin_add(GST_BIN(p->pipeline), gst_object_ref(sink));
+	sinkpad = gst_element_get_static_pad(sink, "sink");
 	gst_pad_link(pad, sinkpad);
 	gst_element_sync_state_with_parent(sink);
 	gst_object_unref(sinkpad);
 	gst_object_unref(sink);
 }
 
+static void _gst_egueb_document_buffer_free(void *data, void *user_data)
+{
+	GstBuffer *buffer = user_data;
+	gst_buffer_unref(buffer);
+}
+
+static void
+_gst_egueb_document_image_fakesink_handoff_cb (GstElement * object,
+		 GstBuffer * buf, GstPad * pad, gpointer data)
+{
+	Gst_Egueb_Document_Pipeline *p = data;
+	GstCaps *caps;
+	const GstStructure *s;
+	Enesim_Buffer *ebuf;
+	Enesim_Buffer_Sw_Data edata;
+	Enesim_Renderer *r;
+	gint width, height;
+
+	caps = gst_buffer_get_caps(buf);
+	s = gst_caps_get_structure(caps, 0);
+	/* get the width and height */
+	gst_structure_get_int(s, "width", &width);
+	gst_structure_get_int(s, "height", &height);
+	gst_caps_unref(caps);
+
+	/* create the surface based on the buffer data */
+	/* the data is not premultiplied, we need to do it */
+	/* TODO we could add the xrgb caps so for jpegs there's no need to premultiply */
+	edata.argb8888.plane0 = GST_BUFFER_DATA(buf);
+	edata.argb8888.plane0_stride = GST_ROUND_UP_4(width * 4);
+
+	ebuf = enesim_buffer_new_data_from(ENESIM_BUFFER_FORMAT_ARGB8888,
+			width, height, EINA_FALSE, &edata, _gst_egueb_document_buffer_free,
+			gst_buffer_ref(buf));
+	p->surface = enesim_surface_new(ENESIM_FORMAT_ARGB8888, width, height);
+
+	r = enesim_renderer_importer_new();
+	enesim_renderer_importer_buffer_set(r, ebuf);
+	enesim_renderer_draw(r, p->surface, ENESIM_ROP_FILL, NULL, 0, 0, NULL);
+	enesim_renderer_unref(r);
+}
+
+static void _gst_egueb_document_image_decodebin2_pad_added_cb (
+		GstElement *src, GstPad *pad, gpointer data)
+{
+	Gst_Egueb_Document_Pipeline *p = data;
+	GstPadLinkReturn linked;
+	GstElement *ffmpegcolorspace;
+	GstElement *capsfilter;
+	GstElement *sink;
+	GstPad *sinkpad;
+	GstPad *srcpad;
+	GstCaps *caps;
+
+	/* connect a ffmpegcolorspace to output on the enesim format */
+	ffmpegcolorspace = gst_element_factory_make("ffmpegcolorspace", NULL);
+
+	/* only allow rgb final data */
+	capsfilter = gst_element_factory_make("capsfilter", NULL);
+	caps = gst_caps_new_simple("video/x-raw-rgb", 
+			"bpp", G_TYPE_INT, 32,
+			"depth", G_TYPE_INT, 32,
+			"endianness", G_TYPE_INT, G_BIG_ENDIAN,
+			"alpha_mask", G_TYPE_INT, 0x000000ff,
+			"red_mask", G_TYPE_INT, 0x0000ff00,
+			"green_mask", G_TYPE_INT, 0x00ff0000,
+			"blue_mask", G_TYPE_INT, 0xff000000,
+			NULL);
+	g_object_set(G_OBJECT(capsfilter), "caps", caps, NULL);
+
+	/* connect a fakesink */
+	sink = gst_element_factory_make("fakesink", NULL);
+	g_object_set(sink, "signal-handoffs", TRUE, NULL);
+	/* get the handoff buffer and put it on an enesim stream */
+	g_signal_connect(G_OBJECT (sink), "handoff",
+			G_CALLBACK (_gst_egueb_document_image_fakesink_handoff_cb),
+			p);
+
+	/* start linking the elements */
+	gst_bin_add_many(GST_BIN(p->pipeline),
+			gst_object_ref(ffmpegcolorspace),
+			gst_object_ref(capsfilter),
+			gst_object_ref(sink), NULL);
+
+	sinkpad = gst_element_get_static_pad(ffmpegcolorspace, "sink");
+	linked = gst_pad_link(pad, sinkpad);
+	gst_object_unref(sinkpad);
+
+	if (linked != GST_PAD_LINK_OK) {
+		gst_bin_remove(GST_BIN(p->pipeline), ffmpegcolorspace);
+		gst_bin_remove(GST_BIN(p->pipeline), capsfilter);
+		gst_bin_remove(GST_BIN(p->pipeline), sink);
+
+		gst_element_set_state(ffmpegcolorspace, GST_STATE_NULL);
+		gst_element_set_state(capsfilter, GST_STATE_NULL);
+		gst_element_set_state(sink, GST_STATE_NULL);
+		p->done = TRUE;
+		goto done;
+	}
+
+	srcpad = gst_element_get_static_pad(ffmpegcolorspace, "src");
+	sinkpad = gst_element_get_static_pad(capsfilter, "sink");
+	gst_pad_link(srcpad, sinkpad);
+	gst_object_unref(sinkpad);
+	gst_object_unref(srcpad);
+
+	srcpad = gst_element_get_static_pad(capsfilter, "src");
+	sinkpad = gst_element_get_static_pad(sink, "sink");
+	gst_pad_link(srcpad, sinkpad);
+	gst_object_unref(sinkpad);
+	gst_object_unref(srcpad);
+
+	gst_element_sync_state_with_parent(ffmpegcolorspace);
+	gst_element_sync_state_with_parent(capsfilter);
+	gst_element_sync_state_with_parent(sink);
+done:
+	gst_object_unref(ffmpegcolorspace);
+	gst_object_unref(capsfilter);
+	gst_object_unref(sink);
+}
+
+static void _gst_egueb_document_image_appsrc_need_data_cb (
+		GstElement *src, guint size, gpointer data)
+{
+	Gst_Egueb_Document_Pipeline *p = data;
+	GstFlowReturn ret;
+	GstBuffer *buf;
+
+	if (p->buffer_pushed)
+	{
+		DBG("Buffer already pushed, pushing the EOS");
+		g_signal_emit_by_name (src, "end-of-stream", &ret);
+		return;
+	}
+
+	DBG("Pushing the image to decode");
+	/* push the stream */
+	buf = gst_buffer_new();
+	GST_BUFFER_DATA(buf) = (gchar *)p->stream_data;
+	GST_BUFFER_SIZE(buf) = p->stream_length;
+
+	/* push the buffer and the end of stream at once */
+	g_signal_emit_by_name (src, "push-buffer", buf, &ret);
+	p->buffer_pushed = TRUE;
+}
 /*----------------------------------------------------------------------------*
  *                               IO interface                                 *
  *----------------------------------------------------------------------------*/
@@ -105,7 +261,6 @@ static void _gst_egueb_document_feature_io_data_cb(Egueb_Dom_Event *ev, void *da
 	Gst_Egueb_Document_Pipeline pipe;
 	GstElement *pipeline;
 	GstElement *uridecodebin;
-	GstPad *srcpad;
 	Egueb_Dom_Uri uri;
 	Egueb_Dom_String *final_uri = NULL;
 	char *binbuf_data;
@@ -141,8 +296,8 @@ static void _gst_egueb_document_feature_io_data_cb(Egueb_Dom_Event *ev, void *da
 
 	/* setup our own pipe */
 	pipe.pipeline = pipeline;
-	pipe.surface = NULL;
 	pipe.binbuf = eina_binbuf_new();
+	pipe.done = FALSE;
 
 	/* create an uridecodebin and put the uri */
 	/* mark the caps as anything so we can get the data raw as it is stored */
@@ -151,14 +306,14 @@ static void _gst_egueb_document_feature_io_data_cb(Egueb_Dom_Event *ev, void *da
 			"uri", egueb_dom_string_string_get(final_uri),
 			"caps", gst_caps_new_any(), NULL);
 	g_signal_connect (G_OBJECT (uridecodebin), "pad-added",
-		G_CALLBACK (_gst_egueb_document_data_uridecodebin_pad_added_cb),
-		&pipe);
+			G_CALLBACK (_gst_egueb_document_data_uridecodebin_pad_added_cb),
+			&pipe);
 	egueb_dom_string_unref(final_uri);
 	gst_bin_add(GST_BIN(pipeline), uridecodebin);
 
 	/* launch it */
 	gst_element_set_state(pipeline, GST_STATE_PLAYING);
-	_gst_egueb_document_pipeline_process(pipeline);
+	_gst_egueb_document_pipeline_process(&pipe);
 	gst_element_set_state(pipeline, GST_STATE_NULL);
 	gst_object_unref(pipeline);
 
@@ -177,21 +332,64 @@ static void _gst_egueb_document_feature_io_data_cb(Egueb_Dom_Event *ev, void *da
 /* create an image loading pipeline */
 static void _gst_egueb_document_feature_io_image_cb(Egueb_Dom_Event *ev, void *data)
 {
+	Gst_Egueb_Document *thiz = data;
+	Gst_Egueb_Document_Pipeline pipe;
+	GstElement *pipeline;
+	GstElement *appsrc;
+	GstElement *decodebin2;
+	GstPad *srcpad;
+	GstPad *sinkpad;
 	Enesim_Stream *s;
 
 	s = egueb_dom_event_io_stream_get(ev);
 	if (!s) return;
 
-	ERR("Image not implemented yet");
-	/* TODO */
 	/* create the pipeline */
-	
-	/* push the buffers from the stream to the branch, i.e appsrc */
+	pipeline = gst_pipeline_new(NULL);
+
+	/* setup our own pipe */
+	pipe.pipeline = pipeline;
+	pipe.surface = NULL;
+	pipe.stream_data = enesim_stream_mmap(s, &pipe.stream_length);
+	pipe.buffer_pushed = FALSE;
+	pipe.done = FALSE;
+
+	/* create the appsrc element to push the buffers to */
+	appsrc = gst_element_factory_make("appsrc", NULL);
+	g_signal_connect (appsrc, "need-data",
+			G_CALLBACK (_gst_egueb_document_image_appsrc_need_data_cb),
+			&pipe);
+	/* setup the decoder */
+	decodebin2 = gst_element_factory_make("decodebin2", NULL);
+	g_signal_connect(G_OBJECT (decodebin2), "pad-added",
+			G_CALLBACK (_gst_egueb_document_image_decodebin2_pad_added_cb),
+			&pipe);
+
+	/* TODO */
 	/* check the decodebin2 signals that the element is for decoding */
 	/* check the decodebin2 caps of the pad added */
 	/* those must start with an image/foo */
-	/* connect a fakesink or an appsink */
-	/* get the handoff buffer and put it on an enesim surface */
+
+	/* link the appsrc and the decodebin2 */
+	gst_bin_add_many(GST_BIN(pipeline), gst_object_ref(appsrc), gst_object_ref(decodebin2), NULL);
+	srcpad = gst_element_get_static_pad(appsrc, "src");
+	sinkpad = gst_element_get_static_pad(decodebin2, "sink");
+	gst_pad_link(srcpad, sinkpad);
+	gst_object_unref(srcpad);
+	gst_object_unref(sinkpad);
+	gst_object_unref(appsrc);
+	gst_object_unref(decodebin2);
+
+	/* launch it */
+	gst_element_set_state(pipeline, GST_STATE_PLAYING);
+	/* push the buffers from the stream to the branch, i.e appsrc */
+	_gst_egueb_document_pipeline_process(&pipe);
+	gst_element_set_state(pipeline, GST_STATE_NULL);
+	gst_object_unref(pipeline);
+
+	/* finish */
+	egueb_dom_event_io_image_finish(ev, pipe.surface);
+	enesim_stream_munmap(s, pipe.stream_data);
 	enesim_stream_unref(s);
 }
 
