2ff3ccf3e8e81d7793510e28206980d519a780c4
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-10-23 01:15:28 +0200

    Do not try to link with the non existant lib

--- src/modules/Makefile.mk
+++ src/modules/Makefile.mk
@@ -9,11 +9,9 @@ src/modules/gst_egueb_document.c \
 src/modules/gst_egueb.c
 
 src_modules_libgstegueb_la_CFLAGS = \
-$(GST_EGUEB_MODULES_CFLAGS) \
--I$(top_srcdir)/src/lib
+$(GST_EGUEB_MODULES_CFLAGS)
 
 src_modules_libgstegueb_la_LIBADD = \
-$(GST_EGUEB_MODULES_LIBS) \
-$(top_builddir)/src/lib/libgstegueb.la
+$(GST_EGUEB_MODULES_LIBS)
 
 src_modules_libgstegueb_la_LDFLAGS = -no-undefined -module -avoid-version
